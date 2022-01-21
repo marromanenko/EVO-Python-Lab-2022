f93c8b4415df9764b7e7c59ccff4999e66d58c39
marromanenko <88029788+marromanenko@users.noreply.github.com>
2022-01-21 15:50:31 +0200

    Update python_1.py

--- task1/python_1.py
+++ task1/python_1.py
@@ -1,25 +1,21 @@
 def direction(facing, turn):
-    arr = ["NE", "E", "SE", "S", "SW", "W", "NW", "N"]
-    pos = -1
-    for i in range(8):
-        if facing == arr[i]:
-            pos = i
-            break
-    if pos != -1 and turn % 45 == 0:
-        step = int(turn/45)
-        res = pos + step
-        while abs(res)>7:
-            if res>0:
-                res -= 8
-            else:
-                res += 8
-        return arr[res]
-    else: return "check the entered data"
- 
+    try:
+        pos = arr.index(facing)
+        if turn % 45 == 0:
+            step = int(turn/45)
+            res = pos + step
+            while abs(res) > 7:
+                res = (pos + step) % 8
+            return arr[res]
+        else:
+            return "check the entered data"
+    except ValueError:
+        return "check the entered data"
 
+
+arr = ["NE", "E", "SE", "S", "SW", "W", "NW", "N"]
 print(direction("S", 180))
 print(direction("SE", -45))
 print(direction("W", 495))
 print(direction("NE", -90))
 print(direction("M", 100))
-
